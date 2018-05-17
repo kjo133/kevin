ba4aae52cb4e153809397167b52651272f551800
Kevin Olson <kolson@AdministorsMBP2.home>
2018-05-17 13:22:38 -0400

    new portfolio

---
+++ portfolio/dev/v2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kevin Olson</title>
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="../gridStyles.css">
+</head>
+<body>
+	<div id="nav-trigger" class="nav-trigger open">
+		<span class="line"></span>
+		<span class="line"></span>
+		<span class="line"></span>
+	</div>
+	<nav id="nav" class="out">
+		<ul>
+			<li><span class="nav-label">Kevin Olson Music</span></li>
+			<li><span class="nav-label">The Sprinting Turtles</span></li>
+			<li><span class="nav-label">ELC Preschool</span></li>
+			<li><span class="nav-label">MAADDIX</span></li>
+		</ul>
+	</nav>
+	<div id="hero">
+		<div>
+			<img src="../images/computer.png">
+			<h1>Kevin Olson
+				<br />
+				Front-End Developer, Musician
+			</h1>
+			<img src="../images/guitar.png">
+			<p>I like to design/develop websites and play music.  Graduated from Virginia Tech with a degree in Computer Science.  Currently in the Washington DC area.</p>
+		</div>
+	</div>
+
+	<div class="container header" id="header">
+        <div class="main"><a href="http://www.kevinjolson.com">Kevin Olson Music</a></div>
+        <div class="secondary1"><a href="http://www.kevinjolson.com/thesprintingturtles">The Sprinting Turtles</a></div>
+        <div class="secondary2"><a href="http://elcvienna.org/preschool/">Emmanuel Lutheran Preschool</a></div>
+        <div class="secondary3"><a href="http://maaddix.com/dev">MAADDIX</a></div>
+        
+    </div>s
+</body>
+<script src="../js/hamburger.js"></script>
+</html>
